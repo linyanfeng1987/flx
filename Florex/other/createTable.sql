b11226cfc0dbe19b23243ab14d02f2cd0532ea0a
linyanfeng <linyanfeng1987@163.com>
2017-11-12 15:25:33 +0800

    增加task表创建sql

--- Florex/other/createTable.sql
+++ Florex/other/createTable.sql
@@ -1,43 +1,13 @@
-drop table `florex`.`currency_pair_eurusd`;
-CREATE TABLE `florex`.`currency_pair_eurusd` (
-  `startTime` INT NOT NULL,
-  `price` FLOAT NOT NULL,
-  `volume` FLOAT NOT NULL,
-  `timeFormat` VARCHAR(45) NULL,
-  PRIMARY KEY (`startTime`));
-
-drop table `florex`.`currency_pair_usdjpy`;
-CREATE TABLE `florex`.`currency_pair_udsjpy` (
-  `startTime` INT NOT NULL,
-  `price` FLOAT NOT NULL,
-  `volume` FLOAT NOT NULL,
-  `timeFormat` VARCHAR(45) NULL,
-  PRIMARY KEY (`startTime`));
-
-drop table `florex`.`currency_pair_xauusd`;
-CREATE TABLE `florex`.`currency_pair_xauusd` (
-  `startTime` INT NOT NULL,
-  `price` FLOAT NOT NULL,
-  `volume` FLOAT NOT NULL,
-  `timeFormat` VARCHAR(45) NULL,
-  PRIMARY KEY (`startTime`));
-  
-drop table `florex`.`currency_pair_gbpusd`;
-CREATE TABLE `florex`.`currency_pair_gbpusd` (
-  `startTime` INT NOT NULL,
-  `price` FLOAT NOT NULL,
-  `volume` FLOAT NOT NULL,
-  `timeFormat` VARCHAR(45) NULL,
-  PRIMARY KEY (`startTime`));
-  
-drop table `florex`.`currency_pair_audusd`;
-CREATE TABLE `florex`.`currency_pair_audusd` (
-  `startTime` INT NOT NULL,
-  `price` FLOAT NOT NULL,
-  `volume` FLOAT NOT NULL,
-  `timeFormat` VARCHAR(45) NULL,
-  PRIMARY KEY (`startTime`));
-  
 
 
   
+CREATE TABLE `core`.`processtask` (
+  `taskId` VARCHAR(45) NOT NULL,
+  `rateName` VARCHAR(20) NULL,
+  `rateType` INT NULL,
+  `startTime` INT NULL,
+  `endTime` INT NULL,
+  `processName` VARCHAR(20) NULL,
+  `paramter` VARCHAR(100) NULL,
+  `status` INT NULL,
+  PRIMARY KEY (`taskId`));
